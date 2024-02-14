691ca09df1265b0f93823a88dd4ce7ef91961b71
joao j <joao.filgueiras.ext@dasa.com.br>
2024-02-14 00:32:56 -0300

    add vercel.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const dotenv = require("dotenv");
+
 const { merge } = require("webpack-merge");
+
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { EnvironmentPlugin } = require("webpack");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "rp";
+
+  const { parsed } = dotenv.config();
+  const environment = { ...parsed };
+
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
@@ -15,12 +23,14 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     plugins: [
+      new EnvironmentPlugin(environment),
       new HtmlWebpackPlugin({
         inject: false,
         template: "src/index.ejs",
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
+          environment: { ...environment }
         },
       }),
     ],
